8bdb7fa746a7d780ec628b53b90ef0d31ca8e329
Radim Sückr <radim.suckr@gmail.com>
2020-09-16 13:32:50 +0200

    Basic usage example

---
+++ example/main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  version = "2.70.0"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  cidr_block = "10.0.0.0/24"
+  vpc_id     = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_launch_template" "tpl" {
+  name          = "tpl"
+  image_id      = "ami-06c755ec615b860ac"
+  instance_type = "t3.nano"
+}
+
+resource "aws_autoscaling_group" "grp" {
+  max_size         = 1
+  min_size         = 1
+  desired_capacity = 1
+
+  vpc_zone_identifier = ["${aws_subnet.subnet.id}"]
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_name = "${aws_launch_template.tpl.name}"
+      }
+    }
+  }
+}
+
+module "refresh" {
+  source = "../"
+
+  launch_template_name   = "${aws_launch_template.tpl.name}"
+  autoscaling_group_name = "${aws_autoscaling_group.grp.name}"
+}
+
+output "asg_arn" {
+  value = "${module.refresh.asg_arn}"
+}
+
+output "lt_arn" {
+  value = "${module.refresh.lt_arn}"
+}
+
+output "lt_id" {
+  value = "${module.refresh.lt_id}"
+}
+
+output "lt_name" {
+  value = "${module.refresh.lt_name}"
+}
